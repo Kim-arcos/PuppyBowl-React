e6a366bde7c3150e1221bd278bed2179cfc621f0
Kim-arcos <kimarcos16@gmail.com>
2024-02-07 16:26:04 -0700

    created the search bar code

--- src/component/PlayerList.jsx
+++ src/component/PlayerList.jsx
@@ -1,22 +1,48 @@
 import { useEffect, useState } from "react";
+import PlayerDetails from "./PlayerDetails";
 
-
-
-function PlayerList(props) {
+const PlayerList = () => {
     const [players, setPlayers] = useState([]);
     const [selectedPlayer, setSelectedPlayer] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredPlayers, setFilteredPlayers] = useState([]);
 
     useEffect(() => {
-        fetch('https://fsa-puppy-bowl.herokuapp.com/api/2308-acc-et-web-pt-a/players')
-        .then((response) => response.json ())
-        .then((data) => setPlayers(data))
-        .catch((error) => console.log(error));
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2308-acc-et-web-pt-a/players"
+                );
+                const data = await response.json();
+                setPlayers(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
     }, []);
+
+    useEffect(() => {
+        const filtered = players.filter((player) =>
+         player.name.toLowerCase().includes(searchQuery.toLowerCase())
+         );
+         setFilteredPlayers(filtered);
+    }, [searchQuery, players]);
+    
+        const handleSearch = (event) => {
+        const query =event.target.value;
+        setSearchQuery(query);
+    };
 
     return (
         <div>
             <h2>Player List</h2>
-            {players.map((player) => (
+           <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="Search Players"
+            />
+            {filteredPlayers.map((player) => (
                 <div key={player.id}>
                     <h3>{player.name}</h3>
                     <p>Owner: {player.owner}</p>
@@ -24,10 +50,10 @@ function PlayerList(props) {
                     <p>Breed: {player.breed}</p>
                     <p>Position: {player.position}</p>
                     <button onClick={() => setSelectedPlayer(player)}>Details</button>
-               {selectedPlayer && <PlayerDetails player={selectedPlayer} />}
-                </div>
-           ))}
-        </div>
+                    {selectedPlayer && <PlayerDetails player={selectedPlayer} />}
+               </div>
+            ))}
+            </div>
     );
 }
 
