6be7cf78b661315164b28e457ac6f1a35294e9b4
Kim-arcos <kimarcos16@gmail.com>
2024-02-07 20:14:04 -0700

    just passing through

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PlayerList from './component/PlayerList';
 import PlayerDetails from './component/PlayerDetails';
 import PlayerForm from './component/PlayerForm';
+import NavBar from './component/NavBar';
 import './styles.css';
 
 function App() {
